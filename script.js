637832cc53022b907125bc638671e8367ce24398
BAWION <132302507+BAWION@users.noreply.github.com>
2024-02-05 01:46:31 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const skillMap = document.getElementById('skillMap');
+    
+    // Ваш код для создания интерактивной карты здесь
+});
